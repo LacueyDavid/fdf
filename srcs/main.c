be4abacc5dc782a28769c04d756be499c0dd048d
Shiro Vulpes <Vulpes.Shiro@gmail.com>
2023-08-04 19:36:01 +0200

    ok

--- srcs/main.c
+++ srcs/main.c
@@ -6,19 +6,22 @@
 /*   By: dlacuey <dlacuey@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 01:41:32 by dlacuey           #+#    #+#             */
-/*   Updated: 2023/08/01 06:52:09 by dlacuey          ###   ########.fr       */
+/*   Updated: 2023/08/04 19:26:06 by dlacuey          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
-void	init_env(t_data *img, t_vars *vars)
+int	init_env(t_data *img, t_vars *vars)
 {
 	vars->mlx = mlx_init();
+	if (!vars->mlx)
+		return (1);
 	vars->win = mlx_new_window(vars->mlx, WIDTH, HEIGHT, "Fil de fer dlacuey");
 	img->img = mlx_new_image(vars->mlx, WIDTH, HEIGHT);
 	img->addr = mlx_get_data_addr(img->img, &img->bits_per_pixel,
 			&img->line_length, &img->endian);
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -31,7 +34,8 @@ int	main(int argc, char **argv)
 		return (print_str("Error.\n"), 1);
 	if (create_map(&map, argv[1]))
 		return (print_str("Error.\n"), 1);
-	init_env(&img, &vars);
+	if (init_env(&img, &vars))
+		return (print_str("Error.\n"), 1);
 	generate_img(map, &img, vars);
 	free_map(map.map, map.map_size);
 	mlx_destroy_image(vars.mlx, img.img);
